0596c22d618fce30d9d5c5980c0b1eee0de7eb48
sameershrestha333 <sameershrestha333@gmail.com>
2019-01-21 08:03:55 -0500

    Simple String trick

---
+++ src/Section3_ValuesAndReferences/StringTrick.java
@@ -0,0 +1,10 @@
+package Section3_ValuesAndReferences;
+
+public class StringTrick {
+    public static void main(String[] args) {
+        String str = "FORD";
+        str.toLowerCase();
+        //it will not changed exisiting string,
+        System.out.println(str);
+    }
+}
